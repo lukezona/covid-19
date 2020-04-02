124d3b444848775fce4ad0136f571203adc74687
Luca Zonarelli <l.zonarelli@nsi.it>
2020-04-02 19:44:36 +0200

    Rimosso helmet

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const helmet = require('helmet');
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
